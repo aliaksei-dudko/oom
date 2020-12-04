4cba8dab9a52d769eb31483fd960eb11e2c2d4d7
aliakseidudko <aliaksei.dudko@tealium.com>
2020-12-04 12:16:59 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		Context context = Context.newBuilder("js").build();
+		Thread.sleep(20000);
+		try  {
+			final Source source = Source.newBuilder("js", getFunction(), "function").cached(false).build();
+			Value futureResult = context.eval(source);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		} finally {
+			context.close(true);
+//			Thread.sleep(100000);
+		}
+	}
+
+	private static String getFunction() {
+		return "//\n" + "// Allocate a certain size to test if it can be done.\n" + "//\n" + "'use strict';\n" + "\n" + "function alloc(size) {\n"
+				+ "    const numbers = size / 8;\n" + "    const arr = []\n" + "    arr.length = numbers; // Simulate allocation of 'size' bytes.\n"
+				+ "    for (let i = 0; i < numbers; i++) {\n" + "        arr[i] = i;\n" + "    }\n" + "    return arr;\n" + "}\n" + "\n" + "//\n"
+				+ "// Keep allocations referenced so they aren't garbage collected.\n" + "//\n" + "const allocations = [];\n" + "\n" + "//\n"
+				+ "// Allocate successively larger sizes, doubling each time until we hit the limit.\n" + "//\n" + "function allocToMax() {\n" + "\n"
+				+ "    console.log(\"Start\");\n" + "\n" + "    let allocationStep = 1024 * 1024 * 5;\n" + "\n" + "    while (true) {\n"
+				+ "        // Allocate memory.\n" + "        const allocation = alloc(allocationStep);\n" + "\n"
+				+ "        // Allocate and keep a reference so the allocated memory isn't garbage collected.\n"
+				+ "        allocations.push(allocation);\n" + "\n" + "    }\n" + "}\n" + "\n" + "allocToMax();";
+	}
+
+//	private static String getFunction() {
+//		return "eee.fff";
+//	}
+}

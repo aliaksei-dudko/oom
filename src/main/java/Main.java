29ed489d6888d4c88fa28916a34425ebc7f3a0ef
aliakseidudko <aliaksei.dudko@tealium.com>
2020-12-30 12:04:13 +0300

    //java oom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,25 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Source;
-import org.graalvm.polyglot.Value;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		Context context = Context.newBuilder("js").build();
 		Thread.sleep(10000);
-		try  {
+		startInJava();
+		Thread.sleep(300000);
+	}
+
+	public static void startInJsContext() throws InterruptedException {
+		Context context = Context.newBuilder("js").build();
+		try {
 			new Processor().process(context);
 		} finally {
 			context.close(true);
 		}
-		Thread.sleep(100000);
 	}
 
-
+	public static void startInJava() throws InterruptedException {
+		new Processor().process();
+	}
 }
--- src/main/java/Processor.java
+++ src/main/java/Processor.java
@@ -1,19 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 
 public class Processor {
 
+	private List<Integer> bigArray = new ArrayList<>();
+
 	public void process(Context context) {
 		try  {
-			final Source source = Source.newBuilder("js", getFunction(), "function").build();
+			final Source source = Source.newBuilder("js", getOOMFunction(), "function").build();
 			context.eval(source);
 		} catch (Throwable e) {
-			System.out.println("error");
-//			e.getMessage();
+			System.out.println("error in js 1");
 		}
 	}
 
-	private static String getFunction() {
+	public void process() {
+		try  {
+			allocToMax();
+		} catch (Throwable e) {
+			System.out.println("error in java 5");
+		}
+	}
+
+	public void allocToMax() {
+
+		System.out.println("Start");
+
+		int allocationStep = 1024 * 1024 * 5;
+
+		while (true) {
+			List<Integer> allocation = alloc(allocationStep);
+			bigArray.addAll(allocation);
+		}
+	}
+
+	public List<Integer> alloc(int size) {
+		int numbers = size / 8;
+		List<Integer> arr = new ArrayList<>();
+		for (int i = 0; i < numbers; i++) {
+			arr.add(i);
+		}
+		return arr;
+	}
+
+	private static String getOOMFunction() {
 		return "//\n" + "// Allocate a certain size to test if it can be done.\n" + "//\n" + "'use strict';\n" + "\n" + "function alloc(size) {\n"
 				+ "    const numbers = size / 8;\n" + "    const arr = []\n" + "    arr.length = numbers; // Simulate allocation of 'size' bytes.\n"
 				+ "    for (let i = 0; i < numbers; i++) {\n" + "        arr[i] = i;\n" + "    }\n" + "    return arr;\n" + "}\n" + "\n" + "//\n"
@@ -24,5 +57,9 @@ public class Processor {
 				+ "        // Allocate and keep a reference so the allocated memory isn't garbage collected.\n"
 				+ "        allocations.push(allocation);\n"    + "\n" + "    }\n"
 				+ "}\n" + " allocToMax();";
+	}
+
+	private static String getLogFunction() {
+		return "console.log(\"Start easy function\");";
 	}
 }
